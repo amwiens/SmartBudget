bfc7cf07dffdfd47ca3f65b3946b6e0a5001d912
amwiens <aaron@awwebsolutions.com>
2020-07-19 08:51:49 -0500

    Styling the menu.

---
+++ src/SmartBudget.Main/ViewModels/MenuViewModel.cs
@@ -0,0 +1,26 @@
+﻿using Prism.Commands;
+using Prism.Mvvm;
+using Prism.Regions;
+
+namespace SmartBudget.Main.ViewModels
+{
+    public class MenuViewModel : BindableBase
+    {
+        private readonly IRegionManager _regionManager;
+
+        public DelegateCommand<string> NavigateCommand { get; private set; }
+
+        public MenuViewModel(IRegionManager regionManager)
+        {
+            _regionManager = regionManager;
+
+            NavigateCommand = new DelegateCommand<string>(Navigate);
+        }
+
+        private void Navigate(string navigatePath)
+        {
+            if (navigatePath != null)
+                _regionManager.RequestNavigate("Content", navigatePath);
+        }
+    }
+}
