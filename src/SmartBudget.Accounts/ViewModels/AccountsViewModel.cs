7bca966500d9c52e79152e2121077c6e84ef8db2
amwiens <aaron@awwebsolutions.com>
2020-07-21 08:09:05 -0500

    Added graphs.

--- src/SmartBudget.Accounts/ViewModels/AccountsViewModel.cs
+++ src/SmartBudget.Accounts/ViewModels/AccountsViewModel.cs
@@ -1,12 +1,39 @@
-﻿using Prism.Mvvm;
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+
+using Prism.Mvvm;
 using Prism.Regions;
+
+using System.Windows.Media;
 
 namespace SmartBudget.Accounts.ViewModels
 {
     public class AccountsViewModel : BindableBase, INavigationAware
     {
+        private SeriesCollection _cardsBalanceCollection;
+        public SeriesCollection CardsBalanceCollection
+        {
+            get { return _cardsBalanceCollection; }
+            set { SetProperty(ref _cardsBalanceCollection, value); }
+        }
+
+        private SeriesCollection _depositBalanceCollection;
+        public SeriesCollection DepositBalanceCollection
+        {
+            get { return _depositBalanceCollection; }
+            set { SetProperty(ref _depositBalanceCollection, value); }
+        }
+
+        private SeriesCollection _creditBalanceCollection;
+        public SeriesCollection CreditBalanceCollection
+        {
+            get { return _creditBalanceCollection; }
+            set { SetProperty(ref _creditBalanceCollection, value); }
+        }
+
         public AccountsViewModel()
         {
+            GetCardsBalance();
         }
 
         public bool IsNavigationTarget(NavigationContext navigationContext)
@@ -20,6 +47,42 @@ namespace SmartBudget.Accounts.ViewModels
 
         public void OnNavigatedTo(NavigationContext navigationContext)
         {
+        }
+
+        private void GetCardsBalance()
+        {
+            CardsBalanceCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Balance",
+                    Values = new ChartValues<double> { 1400, 1500, 1200, 600, 200 },
+                    PointGeometry = null,
+                    PointForeground = Brushes.Green
+                }
+            };
+
+            DepositBalanceCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Balance",
+                    Values = new ChartValues<double> { 1400, 1500, 1200, 600, 200 },
+                    PointGeometry = null,
+                    PointForeground = Brushes.Yellow
+                }
+            };
+
+            CreditBalanceCollection = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Title = "Balance",
+                    Values = new ChartValues<double> { 1400, 1500, 1200, 600, 200 },
+                    PointGeometry = null,
+                    PointForeground = Brushes.Pink
+                }
+            };
         }
     }
 }
