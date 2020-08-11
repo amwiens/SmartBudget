cd71925e6db4f278ad1810351769a721692e8825
Aaron Wiens <aaron@awwebsolutions.com>
2020-08-11 14:46:25 -0500

    Added toast notifications.

--- src/SmartBudget/ViewModels/ShellWindowViewModel.cs
+++ src/SmartBudget/ViewModels/ShellWindowViewModel.cs
@@ -1,9 +1,14 @@
-﻿using Prism.Mvvm;
+﻿using Prism.Events;
+using Prism.Mvvm;
+
+using SmartBudget.Core.Events;
 
 namespace SmartBudget.ViewModels
 {
     public class ShellWindowViewModel : BindableBase
     {
+        private readonly IEventAggregator _eventAggregator;
+        private readonly ToastViewModel _toastViewModel;
         private string _title = "Smart Budget";
 
         public string Title
@@ -12,8 +17,18 @@ namespace SmartBudget.ViewModels
             set { SetProperty(ref _title, value); }
         }
 
-        public ShellWindowViewModel()
+        public ShellWindowViewModel(IEventAggregator eventAggregator)
         {
+            _eventAggregator = eventAggregator;
+            _toastViewModel = new ToastViewModel();
+
+            eventAggregator.GetEvent<NavigationEvent>().Subscribe(OnNavigationReceived);
+        }
+
+        private void OnNavigationReceived(string obj)
+        {
+            if (obj != "Dashboard")
+                _toastViewModel.ShowInformation("Test");
         }
     }
 }
