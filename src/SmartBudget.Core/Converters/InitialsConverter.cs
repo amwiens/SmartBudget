477dc31349c070aae581211c1569a7a7ea47a62f
Aaron Wiens <aaron@awwebsolutions.com>
2020-08-12 08:49:55 -0500

    Updated transactions and expenses avatar to circles and put in the initials of the name.

---
+++ src/SmartBudget.Core/Converters/InitialsConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+
+namespace SmartBudget.Core.Converters
+{
+    public class InitialsConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string s = value as string;
+            string i = string.Empty;
+
+            if (s != null)
+            {
+                string[] split = s.Split(" ".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string piece in split)
+                {
+                    i += piece[0];
+                }
+            }
+
+            return i.Substring(0, 2);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
